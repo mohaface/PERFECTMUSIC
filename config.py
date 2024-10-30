283afcf3bd4bdf3f5d1da8d5a8f0d63832a52666
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-10-30 17:21:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SARKAROP123/NORMALREPO",
+    "https://github.com/SARKAROP123/PERFECTMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TG_NAME_STYLE/4602")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TG_NAME_STYLE/4368")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+k-mc053zHfEwMTU1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+k-mc053zHfEwMTU1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
