7c3e748dcf97c68bfcf0357b83d27fad7f95e3c2
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-10-30 17:50:39 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -27,8 +27,8 @@ def private_panel(_):
             )
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+        [InlineKeyboardButton(text="❍ 𝐎ᴡɴᴇʀ ❍",url=f"https://t.me/ll_SARKAR_MERA_BABU_ll",
+            ),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
